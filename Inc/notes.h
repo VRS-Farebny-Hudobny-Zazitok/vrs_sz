23775fbbecb885c64fbd71abb3d7951bacd4e0a8
RoXoRkA26 <roxor.zelezny@gmail.com>
2022-11-23 20:33:50 +0100

    Created one Octave of notes

---
+++ Inc/notes.h
@@ -0,0 +1,27 @@
+/*
+ * notes.h
+ *
+ *  Created on: 23. 11. 2022
+ *      Author: jakub
+ */
+
+#ifndef NOTES_H_
+#define NOTES_H_
+
+typedef const static struct
+{
+		const static float A4 		= 		440.00f;
+		const static float Ais4 	=		466.16f;
+		const static float B4	 	=		493.88f;
+		const static float C5	 	=		523.25f;
+		const static float Cis5 	=		554.37f;
+		const static float D5 		=		587.33f;
+		const static float Dis5 	=		622.25f;
+		const static float E5 		=		659.25f;
+		const static float F5	 	=		698.46f;
+		const static float Fis5 	=		739.99f;
+		const static float G5	 	=		783.99f;
+		const static float Gis5 	=		830.61f;
+} Notes;
+
+#endif /* NOTES_H_ */

ab02c525418cd3b92bf424c5f752933dd4612407
RoXoRkA26 <roxor.zelezny@gmail.com>
2022-11-23 20:56:34 +0100

    Add second octave

--- Inc/notes.h
+++ Inc/notes.h
@@ -8,6 +8,15 @@
 #ifndef NOTES_H_
 #define NOTES_H_
 
+#define C4		261.63f
+#define Cis4	277.18f
+#define D4		293.66f
+#define Dis4	311.13f
+#define E4		329.63f
+#define F4		349.23f
+#define Fis4	369.99f
+#define G4		392.00f
+#define Gis4	415.30f
 #define A4		440.00f
 #define Ais4 	466.16f
 #define B4	 	493.88f
@@ -20,5 +29,9 @@
 #define	Fis5 	739.99f
 #define	G5	 	783.99f
 #define	Gis5 	830.61f
+#define A5		880.00f
+#define Ais5	932.33f
+#define B5		987.77f
+#define C6		1046.50f
 
 #endif /* NOTES_H_ */

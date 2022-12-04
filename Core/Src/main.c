0e7298666aa1eb70ee7dc42bbe2ade40ddce9ba3
RoXoRkA26 <roxor.zelezny@gmail.com>
2022-12-04 21:27:56 +0100

    LED breathing effect

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,35 +1,16 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "dma.h"
 #include "tim.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -38,105 +19,20 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-#define LED_MAX 8
-#define USE_BRIGHTNESS 1
-
-uint8_t LED_Data[LED_MAX][4];
-uint8_t LED_Mod[LED_MAX][4];
-int dataSendFlag = 0;
-
-void Set_LED (int LEDnum, int red, int green, int blue)
-{
-	LED_Data[LEDnum][0] = LEDnum;
-	LED_Data[LEDnum][1] = green;
-	LED_Data[LEDnum][2] = red;
-	LED_Data[LEDnum][3] = blue;
-}
-
-#define PI 3.14159265
-
-void Set_Brightness (int brightness)
-{
-#if USE_BRIGHTNESS
-	if (brightness > 45) brightness = 45;
-	for (int i = 0; i < LED_MAX; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			float angle = 90 - brightness;
-			angle = angle * PI;
-			LED_Mod[i][j] = (LED_Data[i][j]) / (tan(angle));
-		}
-	}
-#endif
-}
-
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
-{
-	HAL_TIM_PWM_Stop_DMA(&htim2, TIM_CHANNEL_1);
-	dataSendFlag = 1;
-}
-
-uint16_t pwmData[(24 * LED_MAX) + 50];
-
-void LED_Send(void)
-{
-	uint32_t indx = 0;
-	uint32_t color;
-
-	for (int i = 0; i < LED_MAX; i++)
-	{
-		color = ((LED_Mod[i][1] << 16) | (LED_Mod[i][2] << 8) | LED_Mod[i][3]);
-
-		for (int i = 23; i >= 0; i--)
-		{
-			if (color & (1 << i)) pwmData[indx] = 7;
-			else pwmData[indx] = 3;
-
-			indx++;
-		}
-	}
-
-	for (int i = 0; i < 50; i++)
-	{
-		pwmData[indx] = 0;
-		indx++;
-	}
-
-	HAL_TIM_PWM_Start_DMA(&htim2, TIM_CHANNEL_1, (uint32_t *) pwmData, indx);
-	while(!dataSendFlag);
-	dataSendFlag = 0;
-}
-
-void send(int green, int red, int blue)
-{
-	uint32_t color = (green << 16) | (red << 8) | blue;
-
-	for (int i = 23; i >= 0; i--)
-	{
-		if (color&(1 << i)) pwmData[i] = 7;
-		else pwmData[i] = 3;
-	}
-	HAL_TIM_PWM_Start_DMA(&htim2, TIM_CHANNEL_1, (uint32_t *)pwmData, 24);
-}
 /* USER CODE END 0 */
 
 /**
@@ -146,7 +42,6 @@ void send(int green, int red, int blue)
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -155,36 +50,39 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-  Set_LED(0, 255, 0, 0);
-  Set_LED(1, 0, 255, 0);
-  Set_LED(2, 0, 0, 255);
-  Set_Brightness(45);
-  LED_Send();
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+	int pulse = 0;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	int rising = 1;
+ 	while (1)
+	{
+ 		if (rising) pulse++;
+ 		if (!rising) pulse--;
+
+ 		if (pulse == 101) rising = 0;
+ 		if (pulse == 0) rising = 1;
+
+ 		__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse);
+ 		HAL_Delay(20);
+	}
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
   /* USER CODE END 3 */
 }
 
@@ -203,7 +101,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -213,19 +113,18 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
 /**
@@ -235,11 +134,6 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -254,8 +148,6 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */

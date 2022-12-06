8b64e8cd258a314124728f6598eda6334ab10cce
RoXoRkA26 <roxor.zelezny@gmail.com>
2022-12-06 19:27:55 +0100

    Create testing branch

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -32,18 +32,12 @@
 
 /* USER CODE END 1 */
 
-/** Configure pins as
-        * Analog
-        * Input
-        * Output
-        * EVENT_OUT
-        * EXTI
+/** Pinout Configuration
 */
 void MX_GPIO_Init(void)
 {
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
 }
